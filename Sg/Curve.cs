2cf3e6c134c706750280fbff5b3812ecd708bc81
faridmntsr <nmo.maths1@gmail.com>
2021-11-27 19:10:09 +0100

    Nettoyage du code et renommage de classes

---
+++ Sg/Curve.cs
@@ -0,0 +1,8 @@
+﻿namespace Sg
+{
+    internal abstract class Curve
+    {
+        // la méthode qui renvoie la valeur de la courbe à une date donnée
+        public abstract double ValueAtDate(double date);
+    }
+}
